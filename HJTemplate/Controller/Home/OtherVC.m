61f59ba39457d16bb8fcba6cade9921b501ae299
manshu <manshu@tsign.cn>
2021-06-15 16:02:43 +0800

    些许修改

--- HJTemplate/Controller/Home/OtherVC.m
+++ HJTemplate/Controller/Home/OtherVC.m
@@ -12,15 +12,11 @@
 #import "UIColor+Extend.h"
 #import "UIImage+Extend.h"
 #import "MBProgressHUD+JJ.h"
-#import "HJThread.h"
 
 @interface OtherVC ()<UITableViewDelegate, UITableViewDataSource>
 
 @property (nonatomic, strong) UITableView *tableView;
 @property (nonatomic, copy) NSMutableArray *dataSource;
-@property (strong, nonatomic) HJThread *myThread;
-@property (assign, nonatomic) NSInteger num;
-@property (assign, nonatomic) BOOL stoped;
 
 @end
 
@@ -29,48 +25,11 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    self.num = 0;
-    self.stoped = NO;
     self.title = [NSString stringWithFormat:@"%ld", self.index];
     [self subViewSetup];
     [self.navigationController.navigationBar setBackgroundImage:[UIImage imageWithColor:[UIColor hj_randomColor]] forBarMetrics:UIBarMetricsDefault];
     
-    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"hhh" style:UIBarButtonItemStylePlain target:self action:@selector(rightBarButtonAction)];
-    
-    self.myThread = [[HJThread alloc] initWithTarget:self selector:@selector(testThreadAction) object:nil];
-    self.myThread.name = @"com.hjkl.thread";
-    [self.myThread start];
-}
-- (void)testThreadAction {
-    NSLog(@"%s", __FUNCTION__);
-    NSLog(@"done sub thread method ...");
-    CFRunLoopSourceContext context = {0, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL};
-    CFRunLoopSourceRef source = CFRunLoopSourceCreate(kCFAllocatorDefault, 0, &context);
-    CFRunLoopAddSource(CFRunLoopGetCurrent(), source, kCFRunLoopDefaultMode);
-    
-    CFRunLoopRunInMode(kCFRunLoopDefaultMode, 1.0e10, false);
-//    while (!self.stoped) {
-//        NSLog(@"%@", [NSThread currentThread]);
-//        CFRunLoopRun();
-//    }
-    CFRunLoopRemoveSource(CFRunLoopGetCurrent(), source, kCFRunLoopDefaultMode);
-    CFRelease(source);
-    NSLog(@"done end ...");
-}
-- (void)doSomethingAction {
-    NSLog(@"%s", __FUNCTION__);
-    NSLog(@"done something in sub thread ...");
-}
-- (void)rightBarButtonAction {
-    self.num += 1;
-    if (self.num >= 3) {
-        NSLog(@"done stop ...");
-        self.stoped = YES;
-        CFRunLoopStop(CFRunLoopGetCurrent());
-    } else {
-        NSLog(@"done some ...");
-        [self performSelector:@selector(doSomethingAction) onThread:self.myThread withObject:nil waitUntilDone:NO];
-    }
+//    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"hhh" style:UIBarButtonItemStylePlain target:self action:@selector(rightBarButtonAction)];
 }
 - (void)subViewSetup {
     _tableView = [[UITableView alloc] initWithFrame:self.view.bounds style:UITableViewStylePlain];
